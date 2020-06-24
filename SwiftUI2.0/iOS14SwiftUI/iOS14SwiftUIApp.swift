e419291f3ac9e65502de576355bfb8ee960abeab
RamitSharma991 <RamitsMac@Ramits-MacBook-Pro.local>
2020-06-24 19:26:44 +0530

    SwiftUI 2.0 Initial Reactiion

---
+++ SwiftUI2.0/iOS14SwiftUI/iOS14SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS14SwiftUIApp.swift
+//  iOS14SwiftUI
+//
+//  Created by Ramit sharma on 24/06/20.
+//
+
+import SwiftUI
+
+@main
+struct iOS14SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
